1350c374d94e93cf996d360088901e2f63b8d1d3
Chun Yang <yang2007chun@gmail.com>
2016-02-22 16:07:58 -0600

    bugfix, add nvm

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,4 +1,5 @@
-if type "nvm" > /dev/null; then
+nvm_path="$(brew --prefix nvm)/nvm.sh"
+if test -e $nvm_path; then
   export NVM_DIR=~/.nvm
-  source $(brew --prefix nvm)/nvm.sh
+  source $nvm_path
 fi
