4f3191b7b71024f5b6f18d814d59f347ca4e7ba9
Chun Yang <yang2007chun@gmail.com>
2016-04-14 22:43:26 -0500

    npm short cut

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -5,3 +5,8 @@ if test -e $nvm_path; then
 fi
 
 alias publish='npm version minor && npm publish && git push --follow-tags'
+alias nt='npm test'
+alias nw='npm watch'
+alias nb='npm build'
+alias nl='npm lint'
+alias nf='npm fix'
