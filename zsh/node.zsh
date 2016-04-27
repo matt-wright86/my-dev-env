895d3e3b2ac8a677bd7a566c1af7cfa41ea71e73
Chun Yang <yang2007chun@gmail.com>
2016-04-27 12:05:49 -0500

    publish

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -4,10 +4,11 @@ if test -e $nvm_path; then
   source $nvm_path
 fi
 
-alias publish='npm version minor && npm publish && git push --follow-tags'
+alias publish='npm run build && git add -A && (git commit -m "build" || :) && npm version patch && npm publish && git push --follow-tags'
 alias nt='npm run test'
 alias nw='npm run watch'
 alias nb='npm run build'
+alias nbr='npm run browser'
 alias nl='npm run lint'
 alias nf='npm run fix'
 alias ni='npm install'
