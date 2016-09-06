ad3bf4e90c61b564f8bb52ad5018d4fc56fa6aa9
Chun-Yang <yang2007chun@gmail.com>
2016-09-06 09:12:58 -0500

    enable tmuxinator autocompletion

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -5,6 +5,8 @@ alias tmo='tmuxinator open' # create or open a tmuxinator project
 alias tml='tmux list-sessions'
 alias tmd='tmuxinator delete'
 
+# tmuxinator autocomplition
 fpath=($HOME/.tmuxinator/completion ${fpath})
 autoload -U compinit
 compinit
+plugins=(tmuxinator)
